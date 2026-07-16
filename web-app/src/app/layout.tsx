7b610abbb56b0f77c1eda509e987e05118783b89
Norberto Riccitelli <norberto@dibrand.co>
2026-07-16 15:04:32 -0300

    fix: replace vercel favicon with brand icon and set opengraph image

--- web-app/src/app/layout.tsx
+++ web-app/src/app/layout.tsx
@@ -34,7 +34,6 @@ export default function RootLayout({
     >
       <head>
         <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" rel="stylesheet" />
-        <link href="https://lh3.googleusercontent.com/aida-public/AB6AXuA_O5KHVHjc7_uXKWhCErQj_oVuCUEN3wOQJFQGBYYfstFcVecO63oVw0knHAiXKj11bql7zb0Bw4TOIZHEoGtecB0xTcuZlrlmXhFxRd0V5hfnjBot9U6RSTGHXQA1BYQZy2gx_WSekstViAnrjBrN7vWO1srwwtPv8eAMw7aiHHhKSZ6hS2XJTP2aSErtibJmMNJawsGYXMPYcnLx0PCTnqC5TpFCSFPEhNtyrff_MOZ2-EXQTMYDt8uAE1GrLhmsbQ" rel="icon" type="image/png" />
       </head>
       <body className="min-h-full flex flex-col selection:bg-secondary-container selection:text-on-secondary-container bg-surface-container-lowest text-on-surface">
         <Header />
